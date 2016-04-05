cf56debfe2ccbcf6eefcb15b94779495d6864d3c
Shashank Gupta <shashank91.bits@gmail.com>
2016-04-05 13:32:14 -0500

    Added more features for NER, CoRef and RelEx

--- src/main/java/edu/illinois/cs/cogcomp/cs546ccm2/disjoint/CoRef/LocalClassifier/CoRefFeatureGenerator.java
+++ src/main/java/edu/illinois/cs/cogcomp/cs546ccm2/disjoint/CoRef/LocalClassifier/CoRefFeatureGenerator.java
@@ -6,7 +6,6 @@ import java.util.*;
 import edu.illinois.cs.cogcomp.core.datastructures.Pair;
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
-import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Sentence;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 import edu.illinois.cs.cogcomp.sl.core.AbstractFeatureGenerator;
 import edu.illinois.cs.cogcomp.sl.core.IInstance;
@@ -37,16 +36,15 @@ public class CoRefFeatureGenerator extends AbstractFeatureGenerator implements S
 		String prefix = label.toString();
 		
 		// Feature functions here
-//		features.addAll(globalFeatures(inst));
+		features.addAll(getMentionUnigramFeatures(inst));
+		features.addAll(getMentionBigramFeatures(inst));
+		features.addAll(getContextBagUnigramFeatures(inst, 5));
+		features.addAll(getContextBagBigramFeatures(inst, 5));
+		features.addAll(getPOSContextBagUnigramFeatures(inst, 5));
+		features.addAll(getPOSContextBagBigramFeatures(inst, 5));
+		features.addAll(getSameSentenceFeature(inst));
 		
-//		features.addAll(getMentionUnigramFeatures(inst));
-//		features.addAll(getMentionBigramFeatures(inst));
-//		features.addAll(getContextBagUnigramFeatures(inst, 5));
-//		features.addAll(getContextBagBigramFeatures(inst, 5));
-//		features.addAll(getPOSContextBagUnigramFeatures(inst, 5));
-//		features.addAll(getPOSContextBagBigramFeatures(inst, 5));
-		
-//		features.addAll(getEntityTypeFeatures(inst));
+		features.addAll(getEntityTypeFeatures(inst));
 		
 		features.add(new Pair<String, Double>("BIAS", 1.0));
 		
@@ -59,81 +57,6 @@ public class CoRefFeatureGenerator extends AbstractFeatureGenerator implements S
 		
 		return FeatGenTools.getFeatureVectorFromListPair(featuresWithPrefix, lm);
 	}
-	
-//	public List<Pair<String, Double>> globalFeatures(NerInstance x) {
-//		x.ta.
-//		List<Pair<String, Double>> features = new ArrayList<Pair<String, Double>>();
-//		Sentence sent = x.ta.getSentence(questionSentId);
-//		for(int i=sent.getStartSpan(); i<sent.getEndSpan(); ++i) {
-//			if(x.ta.getToken(i).equals("more") || x.ta.getToken(i).equals("less") ||
-//					x.ta.getToken(i).equals("than")) {
-//				features.add(new Pair<String, Double>("MoreOrLessOrThanPresentInQuestion", 1.0));
-//				break;
-//			}
-//		}
-//		
-//		for(String token : x.schema.questionTokens) {
-//			if(token.equalsIgnoreCase("left")) {
-//				features.add(new Pair<String, Double>("LeftPresentInQuestion", 1.0));
-//				break;
-//			}
-//		}
-//		return features;
-//	}
-	
-//	public List<Pair<String, Double>> perQuantityFeatures(NerInstance x) {
-//		List<Pair<String, Double>> features = new ArrayList<Pair<String, Double>>();
-//		for(Pair<String, Double> feature : perQuantityFeatures(x, x.quantIndex1)) {
-//			features.add(new Pair<String, Double>("1_"+feature.getFirst(), feature.getSecond()));
-//		}
-//		
-//		for(Pair<String, Double> feature : perQuantityFeatures(x, x.quantIndex2)) {
-//			features.add(new Pair<String, Double>("2_"+feature.getFirst(), feature.getSecond()));
-//		}
-//		return features;
-//	}
-	
-//	public List<Pair<String, Double>> pairQuantityFeatures(NerInstance x) {
-//		List<Pair<String, Double>> features = new ArrayList<Pair<String, Double>>();
-//		Constituent verbPhrase1 = x.schema.quantSchemas.get(x.quantIndex1).verbPhrase;
-//		Constituent verbPhrase2 = x.schema.quantSchemas.get(x.quantIndex2).verbPhrase;
-//		String verb1 = x.schema.quantSchemas.get(x.quantIndex1).verb;
-//		String verb2 = x.schema.quantSchemas.get(x.quantIndex2).verb;
-//		if(verbPhrase1.getSpan().equals(verbPhrase2.getSpan())) {
-//			features.add(new Pair<String, Double>("SameVerbInstance", 1.0));
-//		}
-//		if(verb1.equals(verb2)) {
-//			features.add(new Pair<String, Double>("SameVerbForm", 1.0));
-//		}
-//		if(Utils.getValue(x.quantities.get(x.quantIndex1)) > Utils.getValue(
-//				x.quantities.get(x.quantIndex2))) {
-//			features.add(new Pair<String, Double>("Ascending", 1.0));
-//		}
-//		return features;
-//	}
-	
-//	public List<Pair<String, Double>> perQuantityFeatures(NerInstance x, int quantIndex) {
-//		List<Pair<String, Double>> features = new ArrayList<Pair<String, Double>>();
-//		QuantitySchema qSchema = x.schema.quantSchemas.get(quantIndex);
-//		int tokenId = x.ta.getTokenIdFromCharacterOffset(x.quantities.get(quantIndex).start);
-//		features.add(new Pair<String,Double>("Verb_"+qSchema.verb, 1.0));
-//		if(qSchema.quantPhrase.getStartSpan() < qSchema.verbPhrase.getStartSpan()) {
-//			features.add(new Pair<String,Double>("QuantityToTheLeftOfVerb", 1.0));
-//		} else {
-//			features.add(new Pair<String,Double>("QuantityToTheRightOfVerb", 1.0));
-//		}
-//		// Neighboring adverbs or comparative adjectives
-//		for(int i=-3; i<=3; ++i) {
-//			if(tokenId+i<x.ta.size() && tokenId+i>=0 && (
-//					x.posTags.get(tokenId+i).getLabel().startsWith("RB") || 
-//					x.posTags.get(tokenId+i).getLabel().startsWith("JJR"))) {
-//				features.add(new Pair<String, Double>(
-//						"Neighbor_"+x.lemmas.get(tokenId+i), 1.0));
-//			}
-//		}
-//		return features;
-//	}
-	
 	
 	public List<Pair<String, Double>> getEntityTypeFeatures(CoRefInstance x) {
 		List<Pair<String, Double>> feats = new ArrayList<>();
@@ -478,6 +401,27 @@ public class CoRefFeatureGenerator extends AbstractFeatureGenerator implements S
 		for(int i=0; i<targetTokens.size()-1; ++i) {
 			feats.add(new Pair<String, Double>("BigramTarget_" + targetTokens.get(i) + "_" + targetTokens.get(i+1), 1.0));
 		}
+		
+		return feats;
+	}
+	
+	public List<Pair<String, Double>> getSameSentenceFeature(CoRefInstance x) {
+		List<Pair<String, Double>> feats = new ArrayList<>();
+		
+		TextAnnotation ta = x.ta;
+		Constituent source = x.mSource; 
+		Constituent target = x.mTarget;
+		
+		int sourceID = ta.getSentenceId(source);
+		int targetID = ta.getSentenceId(target);
+		
+		Boolean isSame = false;
+		
+		if(sourceID == targetID)
+			isSame = true;
+				
+		if(isSame)
+			feats.add(new Pair<String, Double>("SameSentence", 1.0));
 		
 		return feats;
 	}
